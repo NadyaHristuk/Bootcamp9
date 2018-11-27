505c99ced64741285ce2d6f9d05c9458113c6233
Nadin <elhe@i.ua>
2018-11-27 11:06:56 +0200

    add module_8

---
+++ random/index.js
@@ -0,0 +1,5 @@
+let arr = ["Dmytro", "Shapovalov", "Zagorodskiy", "Pecherskiy", "Pasevin" ,"iRoy", "Oxana" , "Karimov", "Roland", "Tatarchuk", "Bezsonov"];
+let rand = Math.floor(Math.random() * arr.length);
+console.log(arr[rand]);
+
+// let arr = ["Dmytro", "Shapovalov", "Zagorodskiy", "Pecherskiy", "Pasevin" ,"iRoy", "Lavrenchuk", "Oxana" , "Karimov", "Roland", "Tatarchuk", "Bezsonov"];
