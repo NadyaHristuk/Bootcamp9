b2f423983bb9a0738b94ae552e6f840f79e013e6
Nadin <elhe@i.ua>
2018-12-18 10:33:58 +0200

    add js

---
+++ JS_Module_1/js_2/index.js
@@ -0,0 +1,13 @@
+// const adminLogin = 'admin';
+// const adminPassword = 'm4ngo1zh4ackz0r';
+// const cancel = alert('Отменено пользователем!');
+// const denide = alert ('Доступ запрещен, неверный логин!');
+// const welcome = alert('Добро пожаловать!');
+
+// let name = prompt("Enter name");
+// let pass = prompt ("Enter pass");
+
+Ваше задание - Сделать скрипт, который запрашивает Имя пользователя, 
+Потом выводит приветствие в котором обращаеться к пользователю по имени,
+ которое тот ввёл.
+ 
